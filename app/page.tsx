53862e14ed0c329f15d23e6b38d4326b7fb98b90
Dani Nugraha <dani.hmng@gmail.com>
2023-10-16 20:32:51 +0700

    chore: clean up landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,32 @@
 import { Metadata } from "next";
-import HomePage from "./home-page";
+import classNames from "classnames";
+import { Kreon } from "next/font/google";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "Next.js",
-  description: "Welcome to Next.js",
+  title: "Aleksandria",
   icons: "/favicon.ico",
 };
 
+const kreonFont = Kreon({
+  subsets: ["latin"],
+});
+
 export default async function Page() {
-  return <HomePage />;
+  return (
+    <div className="container">
+      <main>
+        <h1 className={classNames(kreonFont.className, "text-3xl")}>
+          Aleksandria
+        </h1>
+        <p className="my-3 pb-9">
+          <Link href="/dashboard">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">
+              Open
+            </button>
+          </Link>
+        </p>
+      </main>
+    </div>
+  );
 }
