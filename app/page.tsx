62c0f13313ce7ab50b7f0d4aec02836b9d76b5bc
Dani Nugraha <dani.hmng@gmail.com>
2023-10-28 15:50:13 +0700

    refactor: move landing page centering from global css file to a separate css file
    ...so that it doesn't affect dashboard pages

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import classNames from "classnames";
 import { Kreon } from "next/font/google";
 import OpenButton from "./open-button";
 import { auth } from "@auth/auth";
+import "@styles/dead-center.css";
 
 const kreonFont = Kreon({
   subsets: ["latin"],
@@ -13,9 +14,7 @@ export default async function Page() {
   return (
     <div className="container">
       <main>
-        <h1 className={classNames(kreonFont.className, "text-3xl")}>
-          Aleksandria
-        </h1>
+        <h1 className={classNames(kreonFont.className, "text-3xl")}>Aleksandria</h1>
         <div className="my-3 pb-9">
           <OpenButton session={session} />
         </div>
