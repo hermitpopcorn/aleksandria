c4296f374038045ad7ff17b71d93169783c9bfee
Dani Nugraha <dani.hmng@gmail.com>
2024-01-13 13:36:15 +0700

    feat: make buttons a little bit smaller

--- components/dashboard/base-button.tsx
+++ components/dashboard/base-button.tsx
@@ -3,7 +3,8 @@ import classNames from "classnames";
 export default function BaseButton(props) {
   const { children, className, ...rest } = props;
 
-  const basicClasses = "font-bold py-2 px-4 rounded flex flex-row items-center gap-2";
+  const basicClasses =
+    "font-bold text-sm py-1 px-4 rounded flex flex-row items-center gap-2";
 
   return (
     <button className={classNames(basicClasses, className)} {...rest}>
