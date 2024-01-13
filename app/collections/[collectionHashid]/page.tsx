f5394b20f48240123934e00804fb38074ad12fef
Dani Nugraha <dani.hmng@gmail.com>
2024-01-13 14:47:59 +0700

    feat: add public page for viewing Collections

---
+++ app/collections/[collectionHashid]/page.tsx
@@ -0,0 +1,67 @@
+import prisma from "db";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { cache } from "react";
+import { decodeHashid } from "app/api/hashids";
+import ContentHeader from "@components/dashboard/content-header";
+import DashboardPage from "@components/dashboard/dashboard-page";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import AlphabeticalItemsList from "@components/dashboard/items/alphabetical-list";
+
+const findCollection = cache(async (hashid: string) => {
+  const id = decodeHashid(hashid);
+  try {
+    return await prisma.collection.findFirstOrThrow({
+      where: {
+        id,
+        public: true,
+      },
+      include: {
+        user: true,
+        items: {
+          orderBy: {
+            titleAlphabetic: "asc",
+          },
+        },
+      },
+    });
+  } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError) {
+      notFound();
+    }
+    throw err;
+  }
+});
+
+type Props = {
+  params: { collectionHashid: string };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata | null> {
+  const collection = await findCollection(params.collectionHashid);
+
+  return {
+    title: `${collection?.name} Collection`,
+  };
+}
+
+export default async function CollectionDetailPage({ params }: Props) {
+  const collection = await findCollection(params.collectionHashid);
+
+  return (
+    <DashboardPage>
+      <section className="mb-4">
+        <ContentHeader>
+          {collection.user.name}'s {collection.name} Collection
+        </ContentHeader>
+      </section>
+
+      <section className="mb-4">
+        <AlphabeticalItemsList
+          baseUrl={`/collections/${params.collectionHashid}`}
+          items={collection.items}
+        />
+      </section>
+    </DashboardPage>
+  );
+}
