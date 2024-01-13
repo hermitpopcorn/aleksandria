54e26800f87e53fce9f79309502ce58d31303f61
Dani Nugraha <dani.hmng@gmail.com>
2024-01-13 15:11:35 +0700

    refactor: change hashids to use different salts for different types

--- app/api/hashids.ts
+++ app/api/hashids.ts
@@ -1,6 +1,32 @@
 import Hashids from "hashids";
 
-const hashids = new Hashids(process.env.HASHIDS_SALT ?? "Aleksandria", 6);
+type HashidType = "collections" | "items";
+type HashidsCache = Record<HashidType, Hashids>;
+
+let hashidsCache = {} as HashidsCache;
+
+function getHashids(type: HashidType): Hashids {
+  const checkCache = hashidsCache[type];
+  if (checkCache) {
+    return checkCache;
+  }
+
+  const length = process.env.HASHIDS_LENGTH ? Number(process.env.HASHIDS_LENGTH) : 6;
+  let salt = ((hashidType: HashidType): string => {
+    switch (hashidType) {
+      case "collections":
+        return process.env.HASHIDS_COLLECTIONS_SALT ?? "Aleksandria-Collections";
+      case "items":
+        return process.env.HASHIDS_ITEMS_SALT ?? "Aleksandria-Items";
+      default:
+        return process.env.HASHIDS_SALT ?? "Aleksandria";
+    }
+  })(type);
+
+  const hashids = new Hashids(salt, length);
+  hashidsCache[type] = hashids;
+  return hashids;
+}
 
 export class InvalidHashidError extends Error {
   constructor(message?: string) {
@@ -9,11 +35,11 @@ export class InvalidHashidError extends Error {
   }
 }
 
-export function encodeId(id: number): string {
+function encodeId(hashids: Hashids, id: number): string {
   return hashids.encode(id);
 }
 
-export function decodeHashid(hashid: string): number {
+function decodeHashid(hashids: Hashids, hashid: string): number {
   const decodedNumberLikeArray = hashids.decode(hashid);
 
   if (decodedNumberLikeArray.length != 1) {
@@ -28,4 +54,24 @@ export function decodeHashid(hashid: string): number {
   }
 
   return Number(numberLike);
+}
+
+export function encodeCollectionId(id: number): string {
+  const hashids = getHashids("collections");
+  return encodeId(hashids, id);
+}
+
+export function decodeCollectionHashid(hashid: string): number {
+  const hashids = getHashids("collections");
+  return decodeHashid(hashids, hashid);
+}
+
+export function encodeItemId(id: number): string {
+  const hashids = getHashids("items");
+  return encodeId(hashids, id);
+}
+
+export function decodeItemHashid(hashid: string): number {
+  const hashids = getHashids("items");
+  return decodeHashid(hashids, hashid);
 }
