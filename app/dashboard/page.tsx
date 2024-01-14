158942353aa7c851ace8db360895d9ec18485c00
Dani Nugraha <dani.hmng@gmail.com>
2024-01-14 11:52:04 +0700

    feat: set aria hidden to all dashboard front page content

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,10 +5,13 @@ export default function DashboardPage() {
   const color = "#FAFAFA";
 
   return (
-    <div className="h-screen flex flex-col justify-center items-center gap-4 overflow-hidden">
-      <FaBook size={size} color={color} />
-      <FaGamepad size={size} color={color} />
-      <FaRecordVinyl size={size} color={color} />
+    <div
+      className="h-screen flex flex-col justify-center items-center gap-4 overflow-hidden"
+      aria-hidden
+    >
+      <FaBook size={size} color={color} aria-hidden />
+      <FaGamepad size={size} color={color} aria-hidden />
+      <FaRecordVinyl size={size} color={color} aria-hidden />
     </div>
   );
 }
