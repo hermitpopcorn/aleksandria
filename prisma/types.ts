670c2b25f55d753904212f7fc19b4619f4c96118
Dani Nugraha <dani.hmng@gmail.com>
2024-01-25 20:44:36 +0700

    feat: add additional Item helper types

--- prisma/types.ts
+++ prisma/types.ts
@@ -3,3 +3,9 @@ import { Prisma } from "@prisma/client";
 export type ItemWithCollection = Prisma.ItemGetPayload<{
   include: { collection: true };
 }>;
+
+export type ItemWithInformations = Prisma.ItemGetPayload<{
+  include: { infos: true };
+}>;
+
+export type ItemWithCollectionAndInformations = ItemWithCollection & ItemWithInformations;
